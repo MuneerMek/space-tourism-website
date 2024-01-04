a919ca80da7c3ff507b66069d411c76f40067731
MuneerMek <mmekraz781@proton.me>
2024-01-03 22:05:15 -0500

    converted html base files to jsx

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React from "react";
 import "./App.css";
+import Home from "./Home";
 
-const App = () => {
-  return (
-    <body>
-      00 Home 01 Destination 02 Crew 03 Technology So, you want to travel to
-      Space Let's face it; if you want to go to space, you might as well
-      genuinely go to outer space and not hover kind of on the edge of it. Well
-      sit back, and relax because we'll give you a truly out of this world
-      experience! Explore
-    </body>
-  );
-};
+const App = () => <Home />;
 
 export default App;
