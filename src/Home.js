1a3485adb4076e8d6d9d06b0e4dbc9a09fa22de7
debian-pc <mmekraz781@proton.me>
2024-01-06 21:16:07 -0500

    Added color utility classes

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,9 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="container grid" style={{ "--gap": "4rem" }}>
-      <div className="box">a</div>
-      <div className="box">b</div>
-      <div className="box">c</div>
-      <div className="box">d</div>
+    <div className="container">
+      <h1>Design System</h1>
+      <section id="colors"></section>
     </div>
   );
 };
