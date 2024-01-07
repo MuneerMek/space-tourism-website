61d4705b82db53d5c3c3f7d9fc206cb0f5b6921e
debian-pc <mmekraz781@proton.me>
2024-01-06 22:10:52 -0500

    replicated design system colors in JSX

--- src/Home.js
+++ src/Home.js
@@ -5,23 +5,68 @@ const Home = () => {
     <div className="container">
       <h1>Design system</h1>
 
-      <div className="bg-light text-dark" style={{ padding: "1rem" }}>
-        light blue bg, with dark text
-      </div>
+      <section id="colors">
+        <h2>
+          <span>01</span> colors
+        </h2>
 
-      <div className="bg-white text-dark" style={{ padding: "1rem" }}>
-        white bg, with dark text
-      </div>
-
-      <div
-        className="text-white"
-        style={{
-          padding: "1rem",
-          backgroundColor: "hsl(var(--clr-light) / .2)",
-        }}
-      >
-        light blue bg with a 20% alpha and white text
-      </div>
+        <div
+          className="grid"
+          style={{
+            gridTemplateColumns: "repeat(3, 1fr)",
+            "--gap": ".75rem 1.5rem",
+          }}
+        >
+          <div className="grid text-white">
+            <div
+              className="bg-dark"
+              style={{ padding: "3rem 1rem 1rem", border: "1px solid white" }}
+            >
+              #0B0D17
+            </div>
+            <div>
+              <p>
+                <span style={{ marginRight: "2rem" }}>RGB</span> 11, 13, 23
+              </p>
+              <p>
+                <span style={{ marginRight: "2rem" }}>HSL</span> 230°, 35%, 7%
+              </p>
+            </div>
+          </div>
+          <div className="grid">
+            <div
+              className="bg-light text-dark"
+              style={{ padding: "3rem 1rem 1rem", border: "1px solid white" }}
+            >
+              #D0D6F9
+            </div>
+            <div>
+              <p className="text-white">
+                <span style={{ marginRight: "2rem" }}>RGB</span> 208, 214, 249
+              </p>
+              <p className="text-white">
+                <span style={{ marginRight: "2rem" }}>HSL</span> 231°, 77%, 90%
+              </p>
+            </div>
+          </div>
+          <div className="grid">
+            <div
+              className="bg-white text-dark"
+              style={{ padding: "3rem 1rem 1rem", border: "1px solid white" }}
+            >
+              #FFFFFF
+            </div>
+            <div>
+              <p className="text-white">
+                <span style={{ marginRight: "2rem" }}>RGB</span> 255, 255, 255
+              </p>
+              <p className="text-white">
+                <span style={{ marginRight: "2rem" }}>HSL</span> 0°, 0%, 100%
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
