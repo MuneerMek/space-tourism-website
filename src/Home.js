cf17dcf5b3134c917950fa9519a69261e743a533
debian-pc <mmekraz781@proton.me>
2024-01-06 21:40:11 -0500

    used inline style for color opacity

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,25 @@ import React from "react";
 const Home = () => {
   return (
     <div className="container">
-      <h1>Design System</h1>
-      <section id="colors"></section>
+      <h1>Design system</h1>
+
+      <div className="bg-light text-dark" style={{ padding: "1rem" }}>
+        light blue bg, with dark text
+      </div>
+
+      <div className="bg-white text-dark" style={{ padding: "1rem" }}>
+        white bg, with dark text
+      </div>
+
+      <div
+        className="text-white"
+        style={{
+          padding: "1rem",
+          backgroundColor: "hsl(var(--clr-light) / .2)",
+        }}
+      >
+        light blue bg with a 20% alpha and white text
+      </div>
     </div>
   );
 };
