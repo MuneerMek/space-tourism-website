cb81950881f668d454bd75c24e5ab703f5dc9a72
debian-pc <mmekraz781@proton.me>
2024-01-07 12:47:13 -0500

    added font styles to react dom

--- src/Design.js
+++ src/Design.js
@@ -3,65 +3,138 @@ import React from "react";
 const Design = () => {
   return (
     <div className="container">
-      <h1>Design system</h1>
+      <h1 className="uppercase">Design system</h1>
 
-      <section id="colors">
-        <h2>
+      <section id="colors" style={{ margin: "4rem 0" }}>
+        <h2 className="uppercase">
           <span>01</span> colors
         </h2>
-
-        <div
-          className="grid"
-          style={{
-            gridTemplateColumns: "repeat(3, 1fr)",
-            "--gap": ".75rem 1.5rem",
-          }}
-        >
-          <div className="grid text-white">
+        <div className="flex">
+          <div style={{ flexGrow: 1 }}>
             <div
-              className="bg-dark"
+              className="bg-dark text-white ff-serif fs-500"
               style={{ padding: "3rem 1rem 1rem", border: "1px solid white" }}
             >
               #0B0D17
             </div>
-            <div>
-              <p>
-                <span style={{ marginRight: "2rem" }}>RGB</span> 11, 13, 23
-              </p>
-              <p>
-                <span style={{ marginRight: "2rem" }}>HSL</span> 230°, 35%, 7%
-              </p>
-            </div>
+            <p>
+              <span className="text-light">RGB</span> 11, 13, 23
+            </p>
+            <p>
+              <span className="text-light">HSL</span> 230°, 35%, 7%
+            </p>
           </div>
-          <div className="grid">
+          <div style={{ flexGrow: 1 }}>
             <div
-              className="bg-light text-dark"
+              className="bg-light text-dark ff-serif fs-500"
               style={{ padding: "3rem 1rem 1rem", border: "1px solid white" }}
             >
               #D0D6F9
             </div>
-            <div className="text-white">
-              <p>
-                <span style={{ marginRight: "2rem" }}>RGB</span> 208, 214, 249
-              </p>
-              <p>
-                <span style={{ marginRight: "2rem" }}>HSL</span> 231°, 77%, 90%
-              </p>
-            </div>
+            <p>
+              <span className="text-light">RGB</span> 208, 214, 249
+            </p>
+            <p>
+              <span className="text-light">HSL</span> 231°, 77%, 90%
+            </p>
           </div>
-          <div className="grid">
+          <div style={{ flexGrow: 1 }}>
             <div
-              className="bg-white text-dark"
+              className="bg-white text-dark ff-serif fs-500"
               style={{ padding: "3rem 1rem 1rem", border: "1px solid white" }}
             >
               #FFFFFF
             </div>
-            <div className="text-white">
-              <p>
-                <span style={{ marginRight: "2rem" }}>RGB</span> 255, 255, 255
+            <p>
+              <span className="text-light">RGB</span> 255, 255, 255
+            </p>
+            <p>
+              <span className="text-light">HSL</span> 0°, 0%, 100%
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section id="typography" style={{ margin: "4rem 0" }}>
+        <h2>
+          <span>02 </span>Typography
+        </h2>
+        <div className="flex">
+          <div style={{ flexBasis: "100%" }}>
+            <div>
+              <p className="text-light">
+                Heading 1 - Bellefair Regular - 150px
               </p>
+              <p className="fs-900 ff-serif uppercase">Earth</p>
+            </div>
+            <div>
+              <p className="text-light">
+                Heading 2 - Bellefair Regular - 100px
+              </p>
+              <p className="fs-800 ff-serif uppercase">Venus</p>
+            </div>
+            <div>
+              <p className="text-light">Heading 3 - Bellefair Regular - 56px</p>
+              <p className="fs-700 ff-serif uppercase">Jupiter & Saturn</p>
+            </div>
+            <div>
+              <p className="text-light">Heading 4 - Bellefair Regular - 32px</p>
+              <p className="fs-600 ff-serif uppercase">
+                Uranus, Neptune, & Pluto
+              </p>
+            </div>
+            <div>
+              <p className="text-light">
+                Heading 5 - Barlow Condensed Regular - 28px - 4.75 Character
+                Space
+              </p>
+              <p className="text-light fs-500 ff-sans-cond letter-spacing-1 uppercase">
+                So, you want to travel to space
+              </p>
+            </div>
+          </div>
+
+          <div style={{ flexBasis: "100%" }}>
+            <div>
+              <p className="text-light">
+                Subheading 1 - Bellefair Regular - 28px
+              </p>
+              <p className="fs-500 uppercase">384,400 km</p>
+            </div>
+            <div>
+              <p className="text-light">
+                Subheading 2 - Barlow Condensed Regular - 14px - 2.35 Character
+                Space
+              </p>
+              <p className="fs-200 ff-sans-cond letter-spacing-3 uppercase">
+                Avg. Distance
+              </p>
+            </div>
+            <div>
+              <p className="text-light">
+                Nav Text - Barlow Condensed Regular - 16px - 2.7 Character Space
+              </p>
+              <p className="fs-300 ff-sans-cond letter-spacing-2 uppercase">
+                Europa
+              </p>
+            </div>
+            <div>
+              <p style={{ color: "hsl(var(--clr-light) / .5)" }}>Body Text</p>
               <p>
-                <span style={{ marginRight: "2rem" }}>HSL</span> 0°, 0%, 100%
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi
+                neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium,
+                ligula sollicitudin laoreet viverra, tortor libero sodales leo,
+                eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo.
+                Suspendisse potenti.Lorem ipsum dolor sit amet, consectetuer
+                adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh
+                nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel,
+                nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor
+                libero sodales leo, eget blandit nunc tortor eu nibh. Nullam
+                mollis. Ut justo. Suspendisse potenti.Lorem ipsum dolor sit
+                amet, consectetuer adipiscing elit. Phasellus hendrerit.
+                Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel,
+                dapibus id, mattis vel, nisi.
               </p>
             </div>
           </div>
