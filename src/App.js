453f51e51e716200af7614cef9239f004fdec021
debian-pc <mmekraz781@proton.me>
2024-02-05 19:03:21 -0500

    finalized mobile nav

--- src/App.js
+++ src/App.js
@@ -5,69 +5,88 @@ import Crew from "./Crew";
 import Destination from "./Destination";
 import Technology from "./Technology";
 
-const App = () => (
-  <>
-    <header className="primary-header flex">
-      <div>
-        <img
-          className="logo"
-          src="assets/shared/logo.svg"
-          alt="space tourism logo"
-        ></img>
-      </div>
-      <button className="mobile-nav-toggle" aria-controls="primary-navigation">
-        <span className="sr-only" aria-expanded="false">
-          Menu
-        </span>
-      </button>
-      <nav>
-        <ul
-          id="primary-navigation"
-          className="primary-navigation underline-indicators flex"
+const App = () => {
+  const mobileNavMenu = () => {
+    const nav = document.querySelector(".primary-navigation");
+    const navToggle = document.querySelector(".mobile-nav-toggle");
+    const visiblity = nav.getAttribute("data-visible");
+    if (visiblity === "false") {
+      nav.setAttribute("data-visible", true);
+      navToggle.setAttribute("aria-expanded", true);
+    } else {
+      nav.setAttribute("data-visible", false);
+      navToggle.setAttribute("aria-expanded", false);
+    }
+  };
+  return (
+    <>
+      <header className="primary-header flex">
+        <div>
+          <img
+            className="logo"
+            src="assets/shared/logo.svg"
+            alt="space tourism logo"
+          ></img>
+        </div>
+        <button
+          onClick={mobileNavMenu}
+          className="mobile-nav-toggle"
+          aria-controls="primary-navigation"
         >
-          <li className="active">
-            <Link
-              to="/"
-              className="text-white uppercase ff-sans-cond fs-300 letter-spacing-2"
-            >
-              <span>00</span>Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/destination"
-              className="text-white uppercase ff-sans-cond fs-300 letter-spacing-2"
-            >
-              <span>01</span>Destination
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/crew"
-              className="text-white uppercase ff-sans-cond fs-300 letter-spacing-2"
-            >
-              <span>02</span>Crew
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/technology"
-              className="text-white uppercase ff-sans-cond fs-300 letter-spacing-2"
-            >
-              <span>03</span>Technology
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/destination" element={<Destination />} />
-      <Route path="/crew" element={<Crew />} />
-      <Route path="/technology" element={<Technology />} />
-    </Routes>
-  </>
-);
+          <span className="sr-only" aria-expanded="false">
+            Menu
+          </span>
+        </button>
+        <nav>
+          <ul
+            id="primary-navigation"
+            data-visible="false"
+            className="primary-navigation underline-indicators flex"
+          >
+            <li className="active">
+              <Link
+                to="/"
+                className="text-white uppercase ff-sans-cond fs-300 letter-spacing-2"
+              >
+                <span>00</span>Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/destination"
+                className="text-white uppercase ff-sans-cond fs-300 letter-spacing-2"
+              >
+                <span>01</span>Destination
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/crew"
+                className="text-white uppercase ff-sans-cond fs-300 letter-spacing-2"
+              >
+                <span>02</span>Crew
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/technology"
+                className="text-white uppercase ff-sans-cond fs-300 letter-spacing-2"
+              >
+                <span>03</span>Technology
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/crew" element={<Crew />} />
+        <Route path="/technology" element={<Technology />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
 
