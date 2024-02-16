a07d5788eeb7434c065d98e09d431edd967f1525
debian-pc <mmekraz781@proton.me>
2024-02-15 22:56:46 -0500

    started destination page scripts

--- src/pages/destination/components/Moon.js
+++ src/pages/destination/components/Moon.js
@@ -1,6 +1,27 @@
 import DestinationBg from "./style";
 
 const Moon = () => {
+  const tabSwitch = (e) => {
+    const tabList = document.querySelector('[role="tablist"]');
+    const tabs = tabList.querySelectorAll('[role="tabs"]');
+    console.log(e.keyCode);
+    const keyDownLeft = 37;
+    const keyDownRight = 39;
+
+    // Goal: change the tab index of the current tab to -1
+
+    // const nav = document.querySelector(".primary-navigation");
+    // const navToggle = document.querySelector(".mobile-nav-toggle");
+    // const visiblity = nav.getAttribute("data-visible");
+    // if (visiblity === "false") {
+    //   nav.setAttribute("data-visible", true);
+    //   navToggle.setAttribute("aria-expanded", true);
+    // } else {
+    //   nav.setAttribute("data-visible", false);
+    //   navToggle.setAttribute("aria-expanded", false);
+    // }
+  };
+
   return (
     <>
       <DestinationBg />
@@ -12,7 +33,7 @@ const Moon = () => {
           <span aria-hidden="true">01</span> Pick your destination
         </h1>
 
-        <picture>
+        <picture className="">
           <source
             srcSet="assets/destination/image-moon.webp"
             type="image/webp"
@@ -20,27 +41,71 @@ const Moon = () => {
           <img src="assets/destination/image-moon.png" alt="the moon" />
         </picture>
 
-        <div className="tab-list underline-indicators flex">
+        <picture className="hidden">
+          <source
+            srcSet="assets/destination/image-mars.webp"
+            type="image/webp"
+          />
+          <img src="assets/destination/image-mars.png" alt="the planet mars" />
+        </picture>
+
+        <picture className="hidden">
+          <source
+            srcSet="assets/destination/image-europa.webp"
+            type="image/webp"
+          />
+          <img
+            src="assets/destination/image-europa.png"
+            alt="the planet europa"
+          />
+        </picture>
+
+        <picture className="hidden">
+          <source
+            srcSet="assets/destination/image-titan.webp"
+            type="image/webp"
+          />
+          <img src="assets/destination/image-titan.png" alt="the moon titan" />
+        </picture>
+
+        <div
+          className="tab-list underline-indicators flex"
+          role="tablist"
+          aria-label="destination list"
+          onKeyDown={tabSwitch}
+        >
           <button
             aria-selected="true"
+            role="tab"
+            tabIndex={0}
+            aria-controls="moon-tab"
             className="uppercase text-light ff-sans-cond fs-300 letter-spacing-2"
           >
             Moon
           </button>
           <button
             aria-selected="false"
+            role="tab"
+            tabIndex={-1}
+            aria-controls="mars-tab"
             className="uppercase text-light ff-sans-cond fs-300 letter-spacing-2"
           >
             Mars
           </button>
           <button
             aria-selected="false"
+            role="tab"
+            tabIndex={-1}
+            aria-controls="europa-tab"
             className="uppercase text-light ff-sans-cond fs-300 letter-spacing-2"
           >
             Europa
           </button>
           <button
             aria-selected="false"
+            role="tab"
+            tabIndex={-1}
+            aria-controls="titan-tab"
             className="uppercase text-light ff-sans-cond fs-300 letter-spacing-2"
           >
             Titan
@@ -67,6 +132,76 @@ const Moon = () => {
                 Est. travel time
               </h3>
               <p className="uppercase ff-serif fs-500">3 days</p>
+            </div>
+          </div>
+        </article>
+
+        <article className="hidden destination-info flow">
+          <h2 className="uppercase text-white fs-800 ff-serif">Mars</h2>
+          <p className="text-light">
+            Don't forget to pack your hiking boots. You'll need them to tackle
+            Olympus Mons, the tallest planetary mountain in our solar system.
+            It's two and a half times the size of Everest!
+          </p>
+          <div className="destination-meta flex">
+            <div>
+              <h3 className="uppercase ff-sans-cond text-light fs-200 letter-spacing-3">
+                Avg. distance
+              </h3>
+              <p className="uppercase ff-serif fs-500">225 mil. km</p>
+            </div>
+            <div>
+              <h3 className="uppercase ff-sans-cond text-light fs-200 letter-spacing-3">
+                Est. travel time
+              </h3>
+              <p className="uppercase ff-serif fs-500">9 months</p>
+            </div>
+          </div>
+        </article>
+
+        <article className="hidden destination-info flow">
+          <h2 className="uppercase text-white fs-800 ff-serif">Europa</h2>
+          <p className="text-light">
+            The smallest of the four Galilean moons orbiting Jupiter, Europa is
+            a winter lover's dream. With an icy surface, it's perfect for a bit
+            of ice skating, curling, hockey, or simple relaxation in your snug
+            wintery cabin.
+          </p>
+          <div className="destination-meta flex">
+            <div>
+              <h3 className="uppercase ff-sans-cond text-light fs-200 letter-spacing-3">
+                Avg. distance
+              </h3>
+              <p className="uppercase ff-serif fs-500">628 mil. km</p>
+            </div>
+            <div>
+              <h3 className="uppercase ff-sans-cond text-light fs-200 letter-spacing-3">
+                Est. travel time
+              </h3>
+              <p className="uppercase ff-serif fs-500">3 years</p>
+            </div>
+          </div>
+        </article>
+
+        <article className="hidden destination-info flow">
+          <h2 className="uppercase text-white fs-800 ff-serif">Titan</h2>
+          <p className="text-light">
+            The only moon known to have a dense atmosphere other than Earth,
+            Titan is a home away from home (just a few hundred degrees colder!).
+            As a bonus, you get striking views of the Rings of Saturn.
+          </p>
+          <div className="destination-meta flex">
+            <div>
+              <h3 className="uppercase ff-sans-cond text-light fs-200 letter-spacing-3">
+                Avg. distance
+              </h3>
+              <p className="uppercase ff-serif fs-500">1.6 bil. km</p>
+            </div>
+            <div>
+              <h3 className="uppercase ff-sans-cond text-light fs-200 letter-spacing-3">
+                Est. travel time
+              </h3>
+              <p className="uppercase ff-serif fs-500">7 years</p>
             </div>
           </div>
         </article>
